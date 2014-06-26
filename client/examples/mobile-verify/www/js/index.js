fdb9537a348a754ff1214a56393eaf861a3df84c
David Dahl <ddahl@nulltxt.se>
2014-06-26 09:33:24 -0500

    Verification works via QR scanning or file picking

--- client/examples/mobile-verify/www/js/index.js
+++ client/examples/mobile-verify/www/js/index.js
@@ -21,7 +21,8 @@ var app = {
   initialize: function() {
     $('#account-login').show();
     $('#username-login').focus();
-    crypton.host = 'encryptrservice.crypton.io';
+    crypton.host = 'nulltxt.se';
+    this.utils =  crypton.fingerprintUtils();
     this.bindEvents();
   },
   // Bind Event Listeners
@@ -32,18 +33,18 @@ var app = {
     document.addEventListener('deviceready', this.onDeviceReady, false);
 
     $('#scan').click(function (){
-      app.scanTest();
+      app.scanQRCode();
     });
 
     $('#get-image').click(function (){
       app.getImage();
     });
 
-    $("register-btn").click(function (){
+    $("#register-btn").click(function (){
       app.createAccount();
     });
 
-    $("login-btn").click(function (){
+    $("#login-btn").click(function (){
       app.login();
     });
   },
@@ -74,45 +75,16 @@ var app = {
     console.log('Received Event: ' + id);
   },
 
-  scanTest: function () {
+  scanQRCode: function () {
     cordova.plugins.barcodeScanner.scan(
       function (result) {
-        alert("We got a barcode\n" +
-              "Result: " + result.text + "\n" +
-              "Format: " + result.format + "\n" +
-              "Cancelled: " + result.cancelled);
+        var userObj = JSON.parse(result.text);
+        app.verifyUser(userObj.username, userObj.fingerprint);
       },
       function (error) {
         alert("Scanning failed: " + error);
       }
     );
-  },
-
-  convertImageToCanvas: function () {
-    var canvas = document.createElement('canvas');
-    canvas.width = 420;
-    canvas.height = 420;
-    var ctx = canvas.getContext("2d");
-    var img = $('#picture')[0];
-    ctx.drawImage(img, 0, 0);
-    $('#debug').append(canvas);
-    return canvas;
-  },
-
-  getQrCodeCanvas: function (canvas) {
-    var ctx = canvas.getContext('2d');
-    var qrCode = ctx.getImageData(210, 205, 200, 200);
-    console.log(qrCode);
-
-    var qrCodeCanvas = document.createElement('canvas');
-    qrCodeCanvas.height = 200;
-    qrCodeCanvas.width = 200;
-    $(qrCodeCanvas).attr({id: 'qr-canvas'}); // magick canvas ID
-    console.log(qrCodeCanvas);
-    var qrCtx = qrCodeCanvas.getContext('2d');
-    qrCtx.putImageData(qrCode, 0, 0);
-
-    return qrCodeCanvas;
   },
 
   getImage: function () {
@@ -123,16 +95,9 @@ var app = {
       largeImage.src = imageURI;
 
       qrcode.callback = function (data) {
-        alert(data);
-        // we have data, cache it locally
-
-        // look up the peer from this data
-
-        // compare the fingerprints
-
-        // tell the user we have a match or not
+        // alert(data);
         var userObj = JSON.parse(data);
-        app.verifyUser(data.username, data.fingerprint);
+        app.verifyUser(userObj.username, userObj.fingerprint);
       };
       try {
         qrcode.decode(imageURI);
@@ -209,6 +174,10 @@ var app = {
     $('#account-login .status').text(m);
   },
 
+  formatFingerprint: function (fingerprint) {
+    return this.utils.createFingerprintArr(fingerprint).join(" ");
+  },
+
   verifyUser: function (username, fingerprint) {
     app.session.getPeer(username, function(err, peer) {
       if (err) {
@@ -216,16 +185,18 @@ var app = {
         return;
       }
 
-      if (peer.fingerprint == fingerprint) {
+      var peerFingerprint = app.formatFingerprint(peer.fingerprint);
+
+      if (peerFingerprint == fingerprint) {
         var conf = 'The server supplied fingerprint for '
                  + username
                  + ' is: \n'
-                 + peer.fingerprint
+                 + peerFingerprint
                  + '\nThe fingerprint from the ID card is :\n'
                  + fingerprint
                  + '\nIt is a MATCH, click OK to verify '
                  + username
-                 + 'as a trusted contact.'
+                 + ' as a trusted contact.'
 
         if (window.confirm(conf)) {
           peer.trust(function (err) {
@@ -240,7 +211,7 @@ var app = {
         alert('The server supplied fingerprint for '
              + username
              + 'is: \n'
-             + peer.fingerprint
+             + peerFingerprint
              + '\nThe fingerprint from the ID card is :\n'
              + fingerprint
              + '\nIt is NOT A MATCH\n'
